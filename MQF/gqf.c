4e4c9d4ec6390289441d3c7521897d1dd4c4784f
mostafa.shokrof <mostafa.shokrof@gmail.com>
2018-07-29 14:23:00 +0200

    fmetada sync

--- MQF/gqf.c
+++ MQF/gqf.c
@@ -262,6 +262,7 @@ static void modify_metadata(QF *cf, uint64_t *metadata, int cnt)
 #else
 	//qf_spin_lock(&cf->mem->metadata_lock, true);
 #endif
+	#pragma omp atomic
 	*metadata = *metadata + cnt;
 //	qf_spin_unlock(&cf->mem->metadata_lock);
 	return;
