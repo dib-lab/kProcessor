6bcaa8fd0ecd7bc45420c59ff6b32d66bc18bebf
Mohamed Abuelanin <mohamed.abuelanin@gmail.com>
2018-12-12 03:09:37 +0200

    add BUILD_STATIC option to kProcessor

---
+++ ThirdParty/MQF/build/CMakeFiles/lMQF.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lMQF.dir/src/gqf.cpp.o"
+  "CMakeFiles/lMQF.dir/src/utils.cpp.o"
+  "liblMQF.pdb"
+  "liblMQF.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lMQF.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
