798dc3d106a1ace2dea476b11673ccd7cff63bf3
Shokrof <mostafa.shokrof@gmail.com>
2019-02-28 00:02:28 +0200

    tests

--- tests/kProcessorLibTests/testkDataFrame.cpp
+++ tests/kProcessorLibTests/testkDataFrame.cpp
@@ -1,4 +1,5 @@
 #include "testkDataframe.h"
+//add the new kDataframes here to be tested
 using MyTypes = ::testing::Types<kDataFrameMQF,kDataFrameMAP>;
 TYPED_TEST_SUITE(kDataFrameTest, MyTypes);
 
@@ -7,5 +8,13 @@ TYPED_TEST(kDataFrameTest,emptySize)
 
     kDataFrame* kframe=&this->kFrame;
     bool tmp=kframe->empty();
+    EXPECT_EQ(tmp, true);
+}
+
+TYPED_TEST(kDataFrameTest,bracketOperator)
+{
+
+    kDataFrame* kframe=&this->kFrame;
+    kframe.incrementCounter();
     EXPECT_EQ(tmp, true);
 }
