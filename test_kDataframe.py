4825e295a51977ebfffa34933be308e592ac2649
Mohamed Abuelanin <mohamed.abuelanin@gmail.com>
2019-04-04 03:55:08 +0200

    testCases: union, intersect, iteration, save&load

--- test_kDataframe.py
+++ test_kDataframe.py
@@ -16,21 +16,6 @@ class TestKDataFrame(unittest.TestCase):
 
     fastqFiles = ["testData/test.noN.fastq", "testData/test2.noN.fastq", "testData/test2.noN.fastq"]
 
-    def test_instances_types(self):
-        """
-        check the types of the variables
-        """
-        print(self._testMethodName)
-
-        _int_vector = kp.IntVector()
-        self.assertIsInstance(_int_vector, kp.IntVector)
-
-        _kDataFrameMQF = kp.kDataFrameMQF(31)
-        self.assertIsInstance(_kDataFrameMQF, kp.kDataFrameMQF)
-
-        _kDataFrameMAP = kp.kDataFrameMAP(31)
-        self.assertIsInstance(_kDataFrameMAP, kp.kDataFrameMAP)
-
     def test_emptykDataFrame(self):
         print(self._testMethodName)
 
@@ -65,12 +50,14 @@ class TestKDataFrame(unittest.TestCase):
 
         df_mqf = kp.kDataFrameMQF(kSize)
         self.assertTrue(df_mqf.empty())
+
         df_mqf.insert(_kmer, rand_count)
         self.assertFalse(df_mqf.empty())
         self.assertEqual(df_mqf.count(_kmer), rand_count)
 
         df_map = kp.kDataFrameMAP(kSize)
         self.assertTrue(df_map.empty())
+        
         df_map.insert(_kmer, rand_count)
         self.assertFalse(df_map.empty())
         self.assertEqual(df_map.count(_kmer), rand_count)
@@ -81,9 +68,6 @@ class TestKDataFrame(unittest.TestCase):
 
         # Create random kmers list
         kmers_list = generate_kmers(kSize, kmers_no=20)
-
-        # Count inserted kmers for [kDataFrameMQF, kDataFrameMAP]
-        insertedKmers = [0, 0]
 
         # Empty kDataFrames
         kFrames = [kp.kDataFrameMQF(kSize), kp.kDataFrameMAP(kSize)]
@@ -103,7 +87,6 @@ class TestKDataFrame(unittest.TestCase):
         self.assertEqual(insertedKmers[0], len(kmers_list))  # MQF
         self.assertEqual(insertedKmers[1], len(kmers_list))  # MAP
 
-        verifiedKmers = [0, 0]
         # Verify all inserted kmers
         for i in range(len(kFrames)):
             for kmer in kmers_list:
@@ -137,7 +120,7 @@ class TestKDataFrame(unittest.TestCase):
             for kmer in kmers_list:
                 self.assertEqual(kFrames[i].count(kmer[0]), 0)
 
-    def test_insertNTimes(self):
+    def test_iterateOverAllKmers(self):
         print(self._testMethodName)
         kSize = 31
 
@@ -154,7 +137,138 @@ class TestKDataFrame(unittest.TestCase):
             for kmer in kmers_list:
                 self.assertTrue(kFrames[i].insert(kmer[0], kmer[1]))
 
-    
+        # Get all inserted kmers counts
+        inserted_counts = set([kmer[1] for kmer in kmers_list])
+
+        for kFrame in kFrames:
+            it = kFrame.begin()
+            kframe_kmers_counts = set()
+            while(it != kFrame.end()):
+                count = it.getKmerCount()
+                kframe_kmers_counts.add(count)
+                it.next()
+
+            self.assertEqual(len(kframe_kmers_counts.intersection(inserted_counts)), len(inserted_counts))
+
+    def test_saveAndIterateOverAllKmers(self):
+        print(self._testMethodName)
+        kSize = 31
+
+        # Create random kmers list
+        kmers_list = generate_kmers(kSize, kmers_no=20)
+
+        # Empty kDataFrames
+        kFrames = [kp.kDataFrameMQF(kSize), kp.kDataFrameMAP(kSize)]
+        self.assertTrue(kFrames[0].empty())
+        self.assertTrue(kFrames[1].empty())
+
+        # Insert kmers
+        for i in range(len(kFrames)):
+            for kmer in kmers_list:
+                self.assertTrue(kFrames[i].insert(kmer[0], kmer[1]))
+
+        # Get all inserted kmers counts
+        inserted_counts = set([kmer[1] for kmer in kmers_list])
+
+        fileNames = ["tmp.kdataframe.mqf", "tmp.kdataframe.map"]
+
+        for i in range(len(kFrames)):
+            kFrames[i].save(fileNames[i])
+
+        loaded_kFrames = []
+        for i in range(len(kFrames)):
+            loaded_kFrames.append(kp.kDataFrame.load(fileNames[i]))
+
+        for kFrame in loaded_kFrames:
+            it = kFrame.begin()
+            kframe_kmers_counts = set()
+            while(it != kFrame.end()):
+                count = it.getKmerCount()
+                kframe_kmers_counts.add(count)
+                it.next()
+
+            self.assertEqual(len(kframe_kmers_counts.intersection(inserted_counts)), len(inserted_counts))
+
+    def test_unioinTest(self):
+        print(self._testMethodName)
+        kSize = 31
+
+        # Empty kDataFrames
+        ## auto binding to vector<kDataFrame*>
+        kFrames_vec = [kp.kDataFrameMQF(kSize), kp.kDataFrameMAP(kSize)]
+        self.assertTrue(kFrames_vec[0].empty())
+        self.assertTrue(kFrames_vec[1].empty())
+
+        # Create random kmers list
+        kmers_list1 = generate_kmers(kSize, kmers_no=20)
+        inserted_counts1 = set([kmer[1] for kmer in kmers_list1])
+
+        kmers_list2 = generate_kmers(kSize, kmers_no=20)
+        inserted_counts2 = set([kmer[1] for kmer in kmers_list2])
+
+        # Inserting Kmers
+
+        for kmer in kmers_list1:
+            self.assertTrue(kFrames_vec[0].insert(kmer[0], kmer[1]))
+
+        for kmer in kmers_list2:
+            self.assertTrue(kFrames_vec[1].insert(kmer[0], kmer[1]))
+
+        # Apply kFrameUnion
+        union_kFrame = kp.kFrameUnion(kFrames_vec)
+
+        # Total kmers extracted from union kFrames_vec
+        kframe_kmers_counts = set()
+
+        it = union_kFrame.begin()
+        print(inserted_counts1)
+        print(inserted_counts2)
+
+        while(it != union_kFrame.end()):
+            count = it.getKmerCount()
+            kframe_kmers_counts.add(count)
+            print(count)
+            it.next()
+
+        self.assertEqual(len(kframe_kmers_counts), len(inserted_counts1.union(inserted_counts2)))
+
+    def test_intersectTest(self):
+        print(self._testMethodName)
+        kSize = 31
+
+        # Empty kDataFrames
+        ## auto binding to vector<kDataFrame*>
+        kFrames_vec = [kp.kDataFrameMQF(kSize), kp.kDataFrameMQF(kSize)]
+
+        self.assertTrue(kFrames_vec[0].empty())
+        self.assertTrue(kFrames_vec[1].empty())
+
+        # Create random kmers list
+        kmers_list1 = generate_kmers(kSize, kmers_no=20)
+        inserted_kmers1 = set([kmer[0] for kmer in kmers_list1])
+
+        kmers_list2 = generate_kmers(kSize, kmers_no=10)
+
+        # Replicate some kmers from kmers_list1 in kmers_list2 to make sure len(intersection) > 0
+        kmers_list2 += kmers_list1[0:10]
+
+        inserted_kmers2= set([kmer[0] for kmer in kmers_list2])
+
+        print(kmers_list1)
+        print("--------------------------------")
+        print(kmers_list2)
+
+        # Inserting Kmers
+        for kmer in kmers_list1:
+            self.assertTrue(kFrames_vec[0].insert(kmer[0], kmer[1]))
+
+        for kmer in kmers_list2:
+            self.assertTrue(kFrames_vec[1].insert(kmer[0], kmer[1]))
+
+        # Apply kFrameIntersect
+        intersect_kFrame = kp.kFrameIntersect(kFrames_vec)
+
+        self.assertGreater(intersect_kFrame.size(), 1)
 
 
 
