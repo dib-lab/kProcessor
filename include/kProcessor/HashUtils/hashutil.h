38ca3eeac3074fff3740995fc671cc0103fd4b11
Mohamed Abuelanin <mohamed.abuelanin@gmail.com>
2019-07-20 17:04:03 +0200

    add QHasher class

--- include/kProcessor/HashUtils/hashutil.h
+++ include/kProcessor/HashUtils/hashutil.h
@@ -61,6 +61,35 @@ public:
 	string Ihash(uint64_t key);
 };
 
+class QHasher: public Hasher{
+private:
+    uint64_t mask;
+    uint64_t kSize;
+    unsigned int Q = 28;
+    unsigned int key_remainder_bits;
+
+public:
+    QHasher(uint64_t kSize);
+    QHasher(uint64_t kSize, int _Q);
+    Hasher* clone() { return new QHasher(kSize, Q);}
+
+    // To set the Q if not initialized
+    void set_Q(int _Q);
+
+    uint64_t merge_Q_R(uint64_t &_Q, uint64_t &R);
+    void split_Q_R(uint64_t key,uint64_t &_Q,uint64_t &R);
+
+    uint64_t normal_hash(string key);
+    uint64_t normal_hash(uint64_t key);
+    uint64_t normal_Ihash(uint64_t key);
+
+
+
+    uint64_t hash(string key);
+    uint64_t hash(uint64_t key);
+    string Ihash(uint64_t key);
+};
+
 template<class hashFnType>
 class wrapperHasher: public Hasher{
 private:
--- swig_interfaces/HashUtils/hashutil.i
+++ swig_interfaces/HashUtils/hashutil.i
@@ -30,6 +30,27 @@ public:
 	string Ihash(uint64_t key);
 };
 
+class QHasher: public Hasher{
+    public:
+        QHasher(uint64_t kSize);
+        QHasher(uint64_t kSize, int Q);
+        Hasher* clone() { return new QHasher(kSize, Q);}
+
+        // To set the Q if not initialized
+        void set_Q(int Q);
+
+        uint64_t merge_Q_R(uint64_t &_Q, uint64_t &R);
+        void split_Q_R(uint64_t key,uint64_t Q,uint64_t R);
+
+        uint64_t normal_hash(string key);
+        uint64_t normal_hash(uint64_t key);
+        uint64_t normal_Ihash(uint64_t &key);
+
+        uint64_t hash(string key);
+        uint64_t hash(uint64_t key);
+        string Ihash(uint64_t key);
+};
+
 template<class hashFnType>
 class wrapperHasher: public Hasher{
 private:
