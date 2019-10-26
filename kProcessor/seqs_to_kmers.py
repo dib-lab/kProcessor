2cadd5a525349e24b1694870bb50a0c4c4b68859
Mohamed Abuelanin <mohamed.abuelanin@gmail.com>
2019-10-26 22:14:32 +0200

    update sequence parsing docs

--- kProcessor/seqs_to_kmers.py
+++ kProcessor/seqs_to_kmers.py
@@ -1,28 +1,57 @@
-def parseSequences(KD, output):
+def index(kFrame, mode, params, filename, chunk_size, names_fileName):
+    """
+    Perform indexing to a sequences file with predefined kmers decoding mode.
+
+    :param kFrame: the kDataFrame to be filled with the kmers with their colors
+    :type kFrame: :class:`kProcessor.kDataFrame`
+    :param mode: String defines the mode of kmerDecoder to parse sequences.
+    :type mode: str
+    :param params: Sequence Decoding parameters
+    :type params: dict
+    :param filename: Sequence(s) file path
+    :type filename: str
+    :param chunk_size: Number of sequences to parse at once.
+    :type chunk_size: int
+    :param names_fileName: The TSV names file that contains target sequences headers corresponding to their groups.
+    :return: colored kDataFrame with the decoded kmers and their colors.
+    :rtype: :class:`kProcessor.colored_kDataFrame`
+    """
+
+
+def parseSequencesFromFile(kFrame, mode, params, filename, chunk_size):
     """Load the kmers in the input file into the output kDataframe. Input File can be of formats: fastq,fasta.
 
-    .. note:: Parameters of this function are passed-by-reference. So, it returns nothing.
+    .. note:: The kDataFrame of this function are passed-by-reference. So, it returns nothing.
 
-    :param KD: kmerDecoder Object
-    :type KD: kmerDecoder
-    :param output: kDataFrame to be filled with kmers
-    :type output: :class:`kProcessor.kDataFrame`
+    :param kFrame: the kDataFrame to be filled with the kmers with their counts
+    :type kFrame: :class:`kProcessor.kDataFrame`
+    :param mode: String defines the mode of kmerDecoder to parse sequences.
+    :type mode: str
+    :param params: Sequence Decoding parameters
+    :type params: dict
+    :param filename: Sequence(s) file path
+    :type filename: str
+    :param chunk_size: Number of sequences to parse at once.
+    :type chunk_size: int
 
     """
     pass
 
+#void parseSequencesFromString(kDataFrame * frame, string mode, std::map<std::string, int> params, string sequence);
 
-def parseSequencesFromString(KD, sequence, output):
+def parseSequencesFromString(kFrame, mode, params, seq):
     """Load the kmers in the input string into the output kDataframe.
 
-    .. note:: Parameters of this function are passed-by-reference. So, it returns nothing.
+    .. note:: The kDataFrame of this function are passed-by-reference. So, it returns nothing.
 
-    :param KD: kmerDecoder Object
-    :type KD: kmerDecoder
+    :param kFrame: the kDataFrame to be filled with the kmers with their counts
+    :type kFrame: :class:`kProcessor.kDataFrame`
+    :param mode: String defines the mode of kmerDecoder to parse sequences.
+    :type mode: str
+    :param params: Sequence Decoding parameters
+    :type params: dict
     :param sequence: Sequence to be parsed
     :type sequence: string
-    :param output: kDataFrame to be filled with kmers
-    :type output: :class:`kProcessor.kDataFrame`
 
     """
     pass
--- kProcessor/set_functions.py
+++ kProcessor/set_functions.py
@@ -1,15 +1,3 @@
-def index(KD, names_fileName, frame):
-    """
-    Perform indexing to a sequences file with predefined kmers decoding mode.
-
-    :param KD:
-    :param names_fileName: The TSV names file that contains target sequences headers corresponding to their groups.
-    :param frame: :class:`kProcessor.kDataFrame`
-    :return: colored kDataFrame with the decoded kmers and their colors.
-    :rtype: :class:`kProcessor.colored_kDataFrame`
-    """
-
-
 def kFrameUnion(input):
     """
     Calculate the union of the kDataFrames. The result kDataframe will have all the kmers in the input list of kDataframes.
@@ -20,7 +8,7 @@ def kFrameUnion(input):
     :param input: List of kDataFrames
     :type input: list of :class:`kProcessor.kDataFrameMQF`
     :return: New kDataFrame object holding the union of kmers in the kDataFrames list.
-    :rtype: :class:`kDataFrame`
+    :rtype: :class:`kProcessor.kDataFrame`
 
     """
 
