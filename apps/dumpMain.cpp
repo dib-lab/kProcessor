82656c2383dc3663a8199aae4772a2556e282488
Shokrof <mostafa.shokrof@gmail.com>
2020-04-10 16:05:55 -0700

    dump kmers to files to check

--- apps/dumpMain.cpp
+++ apps/dumpMain.cpp
@@ -8,27 +8,19 @@
 #include "Utils/kmer.h"
 #include "Utils/utils.hpp"
 #include <cmath>
+#include "kDataFrame.hpp"
 
 using namespace std;
 
 
-int dump_main(int argc, char *argv[]){
-  CLI::App app;
-  string input_file="";
-  int k;
-  string outputKmers="";
-
-  app.add_option("-i,--input", input_file,
-   "MQF file")->required()->check(CLI::ExistingFile);
-  app.add_option("-k,--kmer-length",k,"kmer length")->required();
-  app.add_option("-o,--output-kmers", outputKmers,
-    "Output in the format of Kmer\tCount. Available only in Exact Counting")->group("I/O");
-
-  CLI11_PARSE(app, argc, argv);
-
-  QF qf;
-  qf_read(&qf,input_file.c_str());
-
-  kProcessor::dumpMQF(&qf,k,outputKmers);
+int main(int argc, char *argv[]){
+  string input=argv[1];
+  kDataFrame* KF=kDataFrame::load(input);
+  auto it=KF->begin();
+  while(it!=KF->end())
+  {
+    cout<<it.getKmer()<<"\t"<<it.getCount()<<"\n";
+    it++;
+  }
   return 0;
 }
