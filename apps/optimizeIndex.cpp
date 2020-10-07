a1563f318bea050658c12e56c308fb8002fe0f32
Shokrof <mostafa.shokrof@gmail.com>
2020-10-07 10:21:39 +0200

    fix bug in testcases

--- apps/optimizeIndex.cpp
+++ apps/optimizeIndex.cpp
@@ -7,75 +7,72 @@
 #include "kDataFrame.hpp"
 #include <vector>
 #include "algorithms.hpp"
+
 using namespace std;
 
-int main(int argc, char *argv[])
-{
-    string inputList=argv[1];
-    string framePath=argv[2];
-    string outputColumn=argv[3];
+int main(int argc, char *argv[]) {
+    string inputList = argv[1];
+    string framePath = argv[2];
+    string outputColumn = argv[3];
     vector<string> filenames;
-    vector<kDataFrame*> frames;
+    vector<kDataFrame *> frames;
 
     string sample;
     ifstream input(inputList);
-    while(input>>sample)
-    {
+    while (input >> sample) {
         filenames.push_back(sample);
     }
 
-    kDataFrame* indexFrame=kDataFrame::load(framePath);
-    ((queryColorColumn*)indexFrame->getDefaultColumn())->explainSize();
+    kDataFrame *indexFrame = kDataFrame::load(framePath);
+    ((queryColorColumn *) indexFrame->getDefaultColumn())->explainSize();
     //((queryColorColumn*)indexFrame->getDefaultColumn())->sortColors();
 
-    uint64_t testedKmers=0;
-    uint64_t failedKmers=0;
-    uint64_t notFoundKmers =0;
+    uint64_t testedKmers = 0;
+    uint64_t failedKmers = 0;
+    uint64_t notFoundKmers = 0;
 
-    queryColorColumn* qColumn=((queryColorColumn*)indexFrame->getDefaultColumn());
-    prefixTrieQueryColorColumn* pColumn=new prefixTrieQueryColorColumn(qColumn);
+    queryColorColumn *qColumn = ((queryColorColumn *) indexFrame->getDefaultColumn());
+    prefixTrieQueryColorColumn *pColumn = new prefixTrieQueryColorColumn(qColumn);
     pColumn->serialize(outputColumn);
 
 
     pColumn->explainSize();
     indexFrame->changeDefaultColumnType(pColumn);
-    indexFrame->save(framePath+".prefixTrie");
-    delete indexFrame;
-    indexFrame=kDataFrame::load(framePath+".prefixTrie");
+   // indexFrame->save(framePath + ".prefixTrie");
+    //delete indexFrame;
+    //indexFrame = kDataFrame::load(framePath + ".prefixTrie");
 
-    for(int i=0;i<filenames.size();i++)
-    {
-      //  cerr<<"Testing "<<filenames[i]+".testkmers"<<endl;
-        ifstream inp(filenames[i]+".testkmers");
+    for (int i = 0; i < filenames.size(); i++) {
+        //  cerr<<"Testing "<<filenames[i]+".testkmers"<<endl;
+        ifstream inp(filenames[i] + ".testkmers");
         string kmer;
         uint64_t count;
-        while(inp>>kmer>>count)
-        {
+        while (inp >> kmer >> count) {
             testedKmers++;
 //            if(kmer!="CATCTTCCAGTACAGTGTTG")
 //                continue;
-            vector<uint32_t> colors=indexFrame->getKmerDefaultColumnValue<vector<uint32_t >, prefixTrieQueryColorColumn >(kmer);
+            vector<uint32_t> colors = indexFrame->getKmerDefaultColumnValue<vector<uint32_t>, prefixTrieQueryColorColumn>(
+                    kmer);
 
-            if(colors.size()==0)
-            {
+            if (colors.size() == 0) {
                 notFoundKmers++;
                 continue;
             }
-            auto colorIt=find(colors.begin(),colors.end(),i);
-            if(colorIt==colors.end())
-            {
-                cerr<<"Error detected in sample #"<<i <<" "<<
-                    filenames[i]<<" at kmer "<<kmer<<" Combination got "<<endl;
-                for(auto c: colors)
-                    cerr<<c <<" ";
-                cerr<<endl;
+            auto colorIt = find(colors.begin(), colors.end(), i);
+            if (colorIt == colors.end()) {
+                cerr << "Error detected in sample #" << i << " " <<
+                     filenames[i] << " at kmer " << kmer << " Combination got " << endl;
+                for (auto c: colors)
+                    cerr << c << " ";
+                cerr << endl;
 
                 indexFrame->changeDefaultColumnType(qColumn);
-                vector<uint32_t> colors=indexFrame->getKmerDefaultColumnValue<vector<uint32_t >, queryColorColumn >(kmer);
-                cerr<<"It should be ";
-                for(auto c: colors)
-                    cerr<<c <<" ";
-                cerr<<endl;
+                vector<uint32_t> colors = indexFrame->getKmerDefaultColumnValue<vector<uint32_t>, queryColorColumn>(
+                        kmer);
+                cerr << "It should be ";
+                for (auto c: colors)
+                    cerr << c << " ";
+                cerr << endl;
                 failedKmers++;
                 indexFrame->changeDefaultColumnType(pColumn);
             }
@@ -83,8 +80,8 @@ int main(int argc, char *argv[])
         }
         inp.close();
     }
-    cout<<"Numbers of tested Kmers = "<<testedKmers<<endl;
-    cout<<"Numbers of non found kmers = "<<notFoundKmers<<endl;
-    cout<<"Numbers of wrong combination = "<<failedKmers<<endl;
+    cout << "Numbers of tested Kmers = " << testedKmers << endl;
+    cout << "Numbers of non found kmers = " << notFoundKmers << endl;
+    cout << "Numbers of wrong combination = " << failedKmers << endl;
     return 0;
 }
