2cc3e9be82579313bcdf7c36e909333a98e351cb
Abuelanin <mabuelanin@gmail.com>
2021-03-03 02:09:09 +0200

    comment runtime libs

--- setup.py
+++ setup.py
@@ -112,7 +112,7 @@ class CustomBuild(build):
 
 
 kProcessor_module = Extension('_kProcessor',
-                              runtime_library_dirs=RUNTIME_LIBRARIES_DIRS,
+                              # runtime_library_dirs=RUNTIME_LIBRARIES_DIRS,
                               library_dirs=LIBRARIES_DIRS,
                               libraries=LIBRARIES,
                               sources=SOURCES,
