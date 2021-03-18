acf96929ba4a7c04c2bd34b84108f08da0ea3f40
Abuelanin <mabuelanin@gmail.com>
2021-03-18 09:01:07 +0200

    :bug: fix indixing bug issue #76

--- src/algorithms.cpp
+++ src/algorithms.cpp
@@ -778,6 +778,10 @@ namespace kProcessor {
         }
 
 
+        flat_hash_map<uint64_t, string> inv_groupNameMap;
+            for (auto &_ : groupNameMap)
+                inv_groupNameMap[_.second] = _.first;
+
         vector<kDataFrameMQF *> frames;
 //        int currIndex = 0;
         string kmer;
@@ -800,8 +804,8 @@ namespace kProcessor {
 
                 auto it = namesMap.find(readName);
                 if (it == namesMap.end()) {
+                    cerr << "WARNING: " << "read " << readName << "doesn't have a group. Please, check the names file." << endl;
                     continue;
-                    // cout << "read " << readName << "dont have group. Please check the group names file." << endl;
                 }
                 string groupName = it->second;
 
@@ -857,10 +861,22 @@ namespace kProcessor {
 
                         colorsCount[currentTag]--;
                         if (colorsCount[currentTag] == 0 && currentTag != 0) {
-                            freeColors.push(currentTag);
-                            legend->erase(currentTag);
-                            if (convertMap.find(currentTag) != convertMap.end())
-                                convertMap.erase(currentTag);
+                            auto _invGroupNameIT = inv_groupNameMap.find(currentTag);
+                            if (_invGroupNameIT == inv_groupNameMap.end()){
+                                freeColors.push(currentTag);
+
+                                vector<uint32_t> colors = legend->find(currentTag)->second;
+                                string colorsString = to_string(colors[0]);
+                                for (unsigned int k = 1; k < colors.size(); k++) {
+                                    colorsString += ";" + to_string(colors[k]);
+                                }
+                                tagsMap.erase(colorsString);
+                                
+                                legend->erase(currentTag);
+                                if (convertMap.find(currentTag) != convertMap.end())
+                                    convertMap.erase(currentTag);
+                            }
+
                         }
                         colorsCount[itc->second]++;
                     }
@@ -874,8 +890,8 @@ namespace kProcessor {
                     }
                 }
                 readID += 1;
-                if (colorsCount[readTag] == 0) {
-                    groupCounter[groupName]--;
+                groupCounter[groupName]--;
+                if (colorsCount[readTag] == 0) {                   
                     if (groupCounter[groupName] == 0) {
                         freeColors.push(readTag);
                         legend->erase(readTag);
