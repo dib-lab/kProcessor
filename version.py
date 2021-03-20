29547eec16d2d39c884f58c66a6fc4b323b600b5
Abuelanin <mabuelanin@gmail.com>
2021-03-20 13:01:27 +0200

    dynamic version script

---
+++ version.py
@@ -0,0 +1,23 @@
+import os
+import subprocess
+
+def get_version():
+    kProcessor_version = 1.1
+    version_tag = str()
+
+    if os.path.isdir(os.path.join(os.path.dirname(os.path.realpath(__file__)), ".git")):
+        commit_hash_short_name = subprocess.getoutput("git rev-parse --short HEAD").split()[0]
+        branch_name = subprocess.getoutput("git rev-parse --abbrev-ref HEAD").split()[0]
+        if branch_name == "master":
+            version_tag = kProcessor_version
+        else:
+            version_tag = branch_name + "_#" + commit_hash_short_name
+
+    else:
+        version_tag = kProcessor_version
+        pass
+
+    return version_tag
+
+
+print(get_version())
