36624c133f41557e738c228a119332b247d4e94d
mr-eyes <mabuelanin@gmail.com>
2021-06-12 14:49:04 +0200

    edit kprocessor2 version

--- version.py
+++ version.py
@@ -22,8 +22,8 @@ def increment_patch_version(patch_version):
     return f"{patch_version[0]}.{patch_version[1]}.{int(patch_version[2]) + 1}.{patch_version[3]}"
 
 
-MAJOR = 1
-MINOR = 2
+MAJOR = 2
+MINOR = 0
 PATCH = 0
 
 
