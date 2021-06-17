8f2d3aa1c258f5ab346965d6f1bce219fec282b3
Shokrof <mostafa.shokrof@gmail.com>
2021-06-17 17:16:10 +0200

    remove test and use its own app

--- apps/index.cpp
+++ apps/index.cpp
@@ -48,50 +48,50 @@ int main(int argc, char *argv[])
     kProcessor::indexPriorityQueue(frames,tmpFolder,output);
     cout<<"Indexing Finished"<<endl;
 
-    // output->save(outPath);
-    //    cout<<"Saving Finished"<<endl;
-    // delete output;
-    // output=kDataFrame::load(outPath);
-    uint64_t testedKmers=0;
-    uint64_t failedKmers=0;
-    uint64_t notFoundKmers =0;
-    unordered_map<uint32_t ,uint32_t > sizeHist;
-     for(int i=0;i<filenames.size();i++)
-     {
-         ifstream inp(filenames[i]+".testkmers");
-         string kmer;
-         uint64_t count;
-         while(inp>>kmer>>count)
-         {
-     	  if(notFoundKmers+failedKmers==1000)
-     	    break;
-             testedKmers++;
-             vector<uint32_t> colors=output->getKmerDefaultColumnValue<vector<uint32_t >, mixVectors >(kmer);
-             sizeHist[colors.size()]++;
-             if(colors.size()==0)
-             {
-                 cout<<filenames[i]<<" KMER "<<kmer<<endl;
-                 notFoundKmers++;
-                 continue;
-             }
-             auto colorIt=find(colors.begin(),colors.end(),i);
-             if(colorIt==colors.end())
-             {
-                 cerr<<"Error detected in sample #"<<i <<" "<<
-                     filenames[i]<<" at kmer "<<kmer<<" Combination got "<<endl;
-                 for(auto c: colors)
-                     cerr<<c <<" ";
-                 cerr<<endl;
-
-                 failedKmers++;
-             }
-
-         }
-         inp.close();
-     }
-     cout<<"Numbers of tested Kmers = "<<testedKmers<<endl;
-     cout<<"Numbers of non found kmers = "<<notFoundKmers<<endl;
-     cout<<"Numbers of wrong combination = "<<failedKmers<<endl;
+//    // output->save(outPath);
+//    //    cout<<"Saving Finished"<<endl;
+//    // delete output;
+//    // output=kDataFrame::load(outPath);
+//    uint64_t testedKmers=0;
+//    uint64_t failedKmers=0;
+//    uint64_t notFoundKmers =0;
+//    unordered_map<uint32_t ,uint32_t > sizeHist;
+//     for(int i=0;i<filenames.size();i++)
+//     {
+//         ifstream inp(filenames[i]+".testkmers");
+//         string kmer;
+//         uint64_t count;
+//         while(inp>>kmer>>count)
+//         {
+//     	  if(notFoundKmers+failedKmers==1000)
+//     	    break;
+//             testedKmers++;
+//             vector<uint32_t> colors=output->getKmerDefaultColumnValue<vector<uint32_t >, mixVectors >(kmer);
+//             sizeHist[colors.size()]++;
+//             if(colors.size()==0)
+//             {
+//                 cout<<filenames[i]<<" KMER "<<kmer<<endl;
+//                 notFoundKmers++;
+//                 continue;
+//             }
+//             auto colorIt=find(colors.begin(),colors.end(),i);
+//             if(colorIt==colors.end())
+//             {
+//                 cerr<<"Error detected in sample #"<<i <<" "<<
+//                     filenames[i]<<" at kmer "<<kmer<<" Combination got "<<endl;
+//                 for(auto c: colors)
+//                     cerr<<c <<" ";
+//                 cerr<<endl;
+//
+//                 failedKmers++;
+//             }
+//
+//         }
+//         inp.close();
+//     }
+//     cout<<"Numbers of tested Kmers = "<<testedKmers<<endl;
+//     cout<<"Numbers of non found kmers = "<<notFoundKmers<<endl;
+//     cout<<"Numbers of wrong combination = "<<failedKmers<<endl;
 
      //    cout<<"sizeHist"<<endl;
      //    for(auto s:sizeHist)
