78e29b44f2fb7101cd50c9da52fa184678ca85d3
Mostafa <mostafa.shokrof@gmail.com>
2021-09-07 15:21:48 +0200

    more documentation

--- include/kProcessor/algorithms.hpp
+++ include/kProcessor/algorithms.hpp
@@ -61,9 +61,10 @@ namespace kProcessor {
 /// Load the kmers in the input string into the output kDataframe.
     void countKmersFromString(kDataFrame *frame, std::map<std::string, int> parse_params, string sequence);
 
-/// Applies a function on all the kmers in the input kDataframe. The output is another kDataframe with the transformed kmers.
+/// Applies a function on each row in the input kDataframe. The output is another kDataframe with the same kmers and new columns defined based on the user defined function.
     kDataFrame *transform(kDataFrame *input,  function<kmerRow (kmerRow i)>);
 
+/// Applies a function on each row in the input kDataframe. the function updates one or more columns in the input kdataframe based on the user defined function.
     void transformInPlace(kDataFrame *input,  function<void (kDataFrameIterator& i)>);
 
 /// filter the kmers in the kdataframe. The output is another kDataframe with the filtered kmers.
