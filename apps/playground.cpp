a29a5ae58f1130cc5d7c659209c5e01cfc5f2276
Mostafa <mostafa.shokrof@gmail.com>
2021-09-16 15:28:06 +0200

    different optimizations

--- apps/playground.cpp
+++ apps/playground.cpp
@@ -7,63 +7,62 @@
 #include <fstream>
 #include <algorithm>
 #include "algorithms.hpp"
+#include <parallel_hashmap/btree.h>
+
+#include <cereal/types/unordered_map.hpp>
+#include <cereal/types/memory.hpp>
+#include <cereal/archives/binary.hpp>
 using namespace std;
 
 
 int main(int argc, char *argv[]){
 
-    string inputColumn=argv[1];
-    prefixTrie *pColumn = new prefixTrie();
-    pColumn->deserialize(inputColumn);
-    pColumn->explainSize();
-    unordered_map<uint32_t,uint32_t> itemCount;
-    for(auto vec:pColumn->edges)
-      {
-	for(auto i :*vec)
-        itemCount[i]++;
-      }
-    deque<pair<uint32_t,uint32_t> > itemCountVec;
-    for(auto i:itemCount)
-      itemCountVec.push_back(make_pair(i.second,i.first));
-    sort(itemCountVec.begin(),itemCountVec.end());
 
-    sdsl::int_vector<> translate(itemCountVec.size()+1);
-    unordered_map<uint32_t,uint32_t> reverse(itemCountVec.size()+1);
-    cout<<"Unique Items = "<<itemCountVec.size()<<endl;
-    uint32_t ii=0;
-    for(;ii<pColumn->noSamples;ii++)
-      {
-	translate[ii]=ii;
-	reverse[ii]=ii;
-      }
-    
-    for(auto i:itemCountVec)
-      {
-	if(i.second < pColumn->noSamples)
-	  continue;
-	translate[ii]=i.second;
-	reverse[i.second]=ii;
-	ii++;
-      }
-    double eSize=0.0;
-    for(auto vec:pColumn->edges)
-      {
-	sdsl::int_vector<> tmp(vec->size());
-	for(uint32_t i=0;i<vec->size();i++)
-	  {
-	    tmp[i]=reverse[(*vec)[i]];
-	  }
-	//sdsl::enc_vector<> tmpCompressed(tmp);
-	sdsl::util::bit_compress(tmp);
-	//sdsl::enc_vector<> tmpCompressed(tmp);
-	eSize += sdsl::size_in_mega_bytes(tmp);
-	
-	  
-        
-      }
-    cout<<"new size = "<<eSize<<"MB"<<endl;
-    
-    delete pColumn;
+    string columnPrefix=argv[1];
+    auto newColor=new deduplicatedColumn<prefixTrie,phmap::btree_map<uint32_t,uint32_t> >();
+    newColor->deserialize(columnPrefix);
+    cout<<"IDS map size = "<<sdsl::size_in_mega_bytes(newColor->values->idsMap)<<" MB"<<endl;
+    sdsl::util::bit_compress(newColor->values->idsMap);
+    cout<<"Bit Compressed IDS map size = "<<sdsl::size_in_mega_bytes(newColor->values->idsMap)<<" MB"<<endl;
+//    uint64_t nkmers=2722829058;
+    uint64_t nkmers=2722829058;
+    uint32_t maximum=0;
+    sdsl::int_vector<32> intVector(nkmers+1);
+    for(auto i:newColor->index)
+    {
+        maximum=max(maximum,i.second);
+        intVector[i.first]=i.second;
+    }
+    deque<pair<double,string> > results;
+    double tmpSize=newColor->index.size()*4*2/(1024*1024);
+    results.push_back(make_pair(tmpSize,"btree_map<uint32_t,uint32_t>"));
+
+    cout<<"Maximum value = "<<maximum<<endl;
+
+    tmpSize=sdsl::size_in_mega_bytes(intVector);
+    results.push_back(make_pair(tmpSize,"Plain Int Vector"));
+    sdsl::util::bit_compress(intVector);
+    tmpSize=sdsl::size_in_mega_bytes(intVector);
+    results.push_back(make_pair(tmpSize,"bit compress Int Vector"));
+    auto  vlcVector=new sdsl::vlc_vector<>(intVector);
+    tmpSize=sdsl::size_in_mega_bytes(*vlcVector);
+    results.push_back(make_pair(tmpSize,"vlc Vector"));
+    delete vlcVector;
+    auto encVector = new sdsl::enc_vector<>(intVector);
+    tmpSize=sdsl::size_in_mega_bytes(*encVector);
+    results.push_back(make_pair(tmpSize,"enc Vector"));
+    delete encVector;
+    auto dacVector=new sdsl::dac_vector<>(intVector);
+    tmpSize=sdsl::size_in_mega_bytes(*dacVector);
+    results.push_back(make_pair(tmpSize,"dac Vector"));
+    delete dacVector;
+
+    sort(results.begin(),results.end());
+    cout<<"Winner: "<<results[0].second<< " size = "<<results[0].first<<" MB"<<endl;
+
+    for(auto r:results)
+        cout<<r.second<<" : "<<r.first<<" MB"<<endl;
+
     return 0;
 
     
