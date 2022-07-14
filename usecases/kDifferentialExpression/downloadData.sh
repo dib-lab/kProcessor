2e4ab3e344a8562445a065a591090c4767153821
Moustafa Shokrof <mshokrof@farm.ucdavis.edu>
2022-07-14 11:51:33 -0700

    use mosue data instead

--- usecases/kDifferentialExpression/downloadData.sh
+++ usecases/kDifferentialExpression/downloadData.sh
@@ -1,5 +1,58 @@
 mkdir -p inputData
 cd inputData
-wget http://genomedata.org/rnaseq-tutorial/HBR_UHR_ERCC_ds_5pc.tar
-tar -xvf HBR_UHR_ERCC_ds_5pc.tar
-wget http://genomedata.org/rnaseq-tutorial/fasta/GRCh38/chr22_with_ERCC92.fa
+#!/usr/bin/env bash
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/DRR252/DRR252175/DRR252175_1.fastq.gz -o DRR252175_Illumina_HiSeq_2500_paired_end_sequencing_of_SAMD00253462_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/DRR252/DRR252175/DRR252175_2.fastq.gz -o DRR252175_Illumina_HiSeq_2500_paired_end_sequencing_of_SAMD00253462_2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/DRR252/DRR252176/DRR252176_1.fastq.gz -o DRR252176_Illumina_HiSeq_2500_paired_end_sequencing_of_SAMD00253463_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/DRR252/DRR252176/DRR252176_2.fastq.gz -o DRR252176_Illumina_HiSeq_2500_paired_end_sequencing_of_SAMD00253463_2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/DRR252/DRR252174/DRR252174_1.fastq.gz -o DRR252174_Illumina_HiSeq_2500_paired_end_sequencing_of_SAMD00253461_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/DRR252/DRR252174/DRR252174_2.fastq.gz -o DRR252174_Illumina_HiSeq_2500_paired_end_sequencing_of_SAMD00253461_2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/DRR252/DRR252177/DRR252177_1.fastq.gz -o DRR252177_Illumina_HiSeq_2500_paired_end_sequencing_of_SAMD00253464_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/DRR252/DRR252177/DRR252177_2.fastq.gz -o DRR252177_Illumina_HiSeq_2500_paired_end_sequencing_of_SAMD00253464_2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/DRR252/DRR252178/DRR252178_1.fastq.gz -o DRR252178_Illumina_HiSeq_2500_paired_end_sequencing_of_SAMD00253465_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/DRR252/DRR252178/DRR252178_2.fastq.gz -o DRR252178_Illumina_HiSeq_2500_paired_end_sequencing_of_SAMD00253465_2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/DRR252/DRR252179/DRR252179_1.fastq.gz -o DRR252179_Illumina_HiSeq_2500_paired_end_sequencing_of_SAMD00253466_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/DRR252/DRR252179/DRR252179_2.fastq.gz -o DRR252179_Illumina_HiSeq_2500_paired_end_sequencing_of_SAMD00253466_2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/DRR252/DRR252181/DRR252181_1.fastq.gz -o DRR252181_Illumina_HiSeq_2500_paired_end_sequencing_of_SAMD00253468_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/DRR252/DRR252181/DRR252181_2.fastq.gz -o DRR252181_Illumina_HiSeq_2500_paired_end_sequencing_of_SAMD00253468_2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/DRR252/DRR252183/DRR252183_1.fastq.gz -o DRR252183_Illumina_HiSeq_2500_paired_end_sequencing_of_SAMD00253470_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/DRR252/DRR252183/DRR252183_2.fastq.gz -o DRR252183_Illumina_HiSeq_2500_paired_end_sequencing_of_SAMD00253470_2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/DRR252/DRR252180/DRR252180_1.fastq.gz -o DRR252180_Illumina_HiSeq_2500_paired_end_sequencing_of_SAMD00253467_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/DRR252/DRR252180/DRR252180_2.fastq.gz -o DRR252180_Illumina_HiSeq_2500_paired_end_sequencing_of_SAMD00253467_2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/DRR252/DRR252182/DRR252182_1.fastq.gz -o DRR252182_Illumina_HiSeq_2500_paired_end_sequencing_of_SAMD00253469_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/DRR252/DRR252182/DRR252182_2.fastq.gz -o DRR252182_Illumina_HiSeq_2500_paired_end_sequencing_of_SAMD00253469_2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/DRR252/DRR252184/DRR252184_1.fastq.gz -o DRR252184_Illumina_HiSeq_2500_paired_end_sequencing_of_SAMD00253471_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/DRR252/DRR252184/DRR252184_2.fastq.gz -o DRR252184_Illumina_HiSeq_2500_paired_end_sequencing_of_SAMD00253471_2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/DRR252/DRR252185/DRR252185_1.fastq.gz -o DRR252185_Illumina_HiSeq_2500_paired_end_sequencing_of_SAMD00253472_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/DRR252/DRR252185/DRR252185_2.fastq.gz -o DRR252185_Illumina_HiSeq_2500_paired_end_sequencing_of_SAMD00253472_2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/DRR252/DRR252187/DRR252187_1.fastq.gz -o DRR252187_Illumina_HiSeq_2500_paired_end_sequencing_of_SAMD00253474_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/DRR252/DRR252187/DRR252187_2.fastq.gz -o DRR252187_Illumina_HiSeq_2500_paired_end_sequencing_of_SAMD00253474_2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/DRR252/DRR252188/DRR252188_1.fastq.gz -o DRR252188_Illumina_HiSeq_2500_paired_end_sequencing_of_SAMD00253475_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/DRR252/DRR252188/DRR252188_2.fastq.gz -o DRR252188_Illumina_HiSeq_2500_paired_end_sequencing_of_SAMD00253475_2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/DRR252/DRR252190/DRR252190_1.fastq.gz -o DRR252190_Illumina_HiSeq_2500_paired_end_sequencing_of_SAMD00253477_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/DRR252/DRR252190/DRR252190_2.fastq.gz -o DRR252190_Illumina_HiSeq_2500_paired_end_sequencing_of_SAMD00253477_2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/DRR252/DRR252191/DRR252191_1.fastq.gz -o DRR252191_Illumina_HiSeq_2500_paired_end_sequencing_of_SAMD00253478_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/DRR252/DRR252191/DRR252191_2.fastq.gz -o DRR252191_Illumina_HiSeq_2500_paired_end_sequencing_of_SAMD00253478_2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/DRR252/DRR252186/DRR252186_1.fastq.gz -o DRR252186_Illumina_HiSeq_2500_paired_end_sequencing_of_SAMD00253473_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/DRR252/DRR252186/DRR252186_2.fastq.gz -o DRR252186_Illumina_HiSeq_2500_paired_end_sequencing_of_SAMD00253473_2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/DRR252/DRR252189/DRR252189_1.fastq.gz -o DRR252189_Illumina_HiSeq_2500_paired_end_sequencing_of_SAMD00253476_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/DRR252/DRR252189/DRR252189_2.fastq.gz -o DRR252189_Illumina_HiSeq_2500_paired_end_sequencing_of_SAMD00253476_2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/DRR252/DRR252192/DRR252192_1.fastq.gz -o DRR252192_Illumina_HiSeq_2500_paired_end_sequencing_of_SAMD00253479_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/DRR252/DRR252192/DRR252192_2.fastq.gz -o DRR252192_Illumina_HiSeq_2500_paired_end_sequencing_of_SAMD00253479_2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/DRR252/DRR252193/DRR252193_1.fastq.gz -o DRR252193_Illumina_HiSeq_2500_paired_end_sequencing_of_SAMD00253480_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/DRR252/DRR252193/DRR252193_2.fastq.gz -o DRR252193_Illumina_HiSeq_2500_paired_end_sequencing_of_SAMD00253480_2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/DRR252/DRR252194/DRR252194_1.fastq.gz -o DRR252194_Illumina_HiSeq_2500_paired_end_sequencing_of_SAMD00253481_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/DRR252/DRR252194/DRR252194_2.fastq.gz -o DRR252194_Illumina_HiSeq_2500_paired_end_sequencing_of_SAMD00253481_2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/DRR252/DRR252195/DRR252195_1.fastq.gz -o DRR252195_Illumina_HiSeq_2500_paired_end_sequencing_of_SAMD00253482_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/DRR252/DRR252195/DRR252195_2.fastq.gz -o DRR252195_Illumina_HiSeq_2500_paired_end_sequencing_of_SAMD00253482_2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/DRR252/DRR252196/DRR252196_1.fastq.gz -o DRR252196_Illumina_HiSeq_2500_paired_end_sequencing_of_SAMD00253483_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/DRR252/DRR252196/DRR252196_2.fastq.gz -o DRR252196_Illumina_HiSeq_2500_paired_end_sequencing_of_SAMD00253483_2.fastq.gz
+
+
+#wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/001/635/GCF_000001635.27_GRCm39/GCF_000001635.27_GRCm39_genomic.fna.gz
+
+wget https://hgdownload.soe.ucsc.edu/goldenPath/mm39/bigZips/mrna.fa.gz
+
+gzip -d mrna.fa.gz
+
+#wget http://genomedata.org/rnaseq-tutorial/fasta/GRCh38/chr22_with_ERCC92.fa
